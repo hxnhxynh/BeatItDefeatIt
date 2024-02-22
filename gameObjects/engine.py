d02d0ed8a3812ac20571d8a56bf61a3033934fa6
Han Huynh <huynhh25@mail.wlu.edu>
2024-02-21 21:56:28 -0500

    Fixed visuals and added instructions.

--- gameObjects/engine.py
+++ gameObjects/engine.py
@@ -12,12 +12,15 @@ class GameEngine(object):
 
     def __init__(self):       
         self.size = vec(*RESOLUTION)
-        self.background = Drawable((0,0), "background.png")
-        self.sequence = Sequence(((RESOLUTION/2)-(SEQUENCE_SIZE[0]/2,16)),
+        self.background = Drawable((0,0), "dasKlubBackground.png")
+        self.sequence = Sequence(((RESOLUTION/2)-(SEQUENCE_SIZE[0]/2,-16)),
                                   NUMARROWS)
         self.points = 0
-        self.font =  pygame.font.SysFont("Arial", 25)
+        self.font =  pygame.font.SysFont("Harlow Solid", 15)
         self.pointDisplay = self.font.render("Score count: 0",
+                                             False,
+                                             (255,255,255))
+        self.instructions = self.font.render("Press the arrow keys in order to earn points!",
                                              False,
                                              (255,255,255))
 
@@ -26,11 +29,12 @@ class GameEngine(object):
         self.background.draw(drawSurface)
         self.sequence.draw(drawSurface)
         drawSurface.blit(self.pointDisplay, (0,0))
-            
+        drawSurface.blit(self.instructions, (RESOLUTION[0]/2-(self.instructions.get_width()//2), RESOLUTION[1]-50))
+
     def handleEvent(self, event):
         self.sequence.handleEvent(event)
         if self.sequence.complete:
-            self.sequence = Sequence(((RESOLUTION/2)-(SEQUENCE_SIZE[0]/2,16)),
+            self.sequence = Sequence(((RESOLUTION/2)-(SEQUENCE_SIZE[0]/2,-16)),
                                   NUMARROWS)
             self.points += 100
             self.pointDisplay = self.font.render("Score count: " + str(self.points),
