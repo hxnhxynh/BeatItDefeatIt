b77730bc75301ef73984a920076b82813f32e09f
Han Huynh <huynhh25@mail.wlu.edu>
2024-03-31 21:01:04 -0400

    Created a player object to represent player character.

--- gameObjects/__init__.py
+++ gameObjects/__init__.py
@@ -2,6 +2,7 @@ from .drawable import *
 from .animated import *
 from .interpolate import *
 from .mobile import *
+from .player import *
 from .sequence import *
 from .timingBar import *
 from .engine import *
---
+++ gameObjects/player.py
@@ -0,0 +1,55 @@
+from . import Mobile
+from FSMs import WalkingFSM, AccelerationFSM
+
+from pygame.locals import *
+import pygame
+import numpy as np
+
+class Player(Mobile):
+    
+    def __init__(self, position):
+        super().__init__(position, "playerSheet.png")
+
+        self.framesPerSecond = 2
+        self.nFrames = 8
+
+        self.nFramesList = {
+            "standing" : 4,
+            "moving" : 8
+        }
+
+        self.rowList = {
+            "standing" : 0,
+            "moving": 1
+        }
+
+        self.framesPerSecondList = {
+            "standing": 4,
+            "moving": 8 
+        }
+
+        self.FSManimated = WalkingFSM(self)
+        self.LR = AccelerationFSM(self, axis=0)
+
+    def handleEvent(self, event):
+      if event.type == KEYDOWN:
+            
+         if event.key == K_LEFT:
+            self.LR.decrease()
+            
+         elif event.key == K_RIGHT:
+            self.LR.increase()
+            
+      elif event.type == KEYUP:
+             
+            
+         if event.key == K_LEFT:
+            self.LR.stop_decrease()
+            
+         elif event.key == K_RIGHT:
+            self.LR.stop_increase()
+    
+    def update(self, seconds): 
+      self.LR.updateState()
+      self.LR.update(seconds)
+      super().update(seconds)
